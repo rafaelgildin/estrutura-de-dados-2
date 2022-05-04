30c5d4b76870d703b05dfe0b9dfeacaccbacbae3
rafaelgildin <rafagildin@gmail.com>
2022-05-03 21:50:00 -0300

    atualizando

--- tarefa-3-AVL/AVL-int/ArvoresAVL.cpp
+++ tarefa-3-AVL/AVL-int/ArvoresAVL.cpp
@@ -1,3 +1,5 @@
+// cd /c/Users/rafaa/rafa/git/estrutura-de-dados-2/tarefa-3-AVL/extras/AVL-final/dados
+// clear && g++ -o ArvoresAVL.o ArvoresAVL.cpp && ./ArvoresAVL.o
 #include <iostream>
 #include <cstdlib>
 using namespace std;
@@ -55,112 +57,20 @@ class Arvore
     public:
 	No *raiz;
     No *noDesbalanceado;
+    No *noPaiDesbalanceado;//novo
 	int testaFB;
 	Arvore()
 	{
 		raiz = NULL;
         noDesbalanceado = NULL;
+        noPaiDesbalanceado=NULL;//novo
 	}
 
     int contarNos(No* atual){
     if(atual == NULL)  return 0;
     else return ( 1 + contarNos(atual->getEsq()) + contarNos(atual->getDir()));
     }
-
-    void Balancear(void){
-        /* Balancear a arvore
-        nosPtr: vetor com os ponteiros da arvore
-        nosChaves: vetor com as chaves da arvore
-        qNos: quantidade de nos na arvore
-        */
-        int i,valor, qNos = contarNos(raiz), q=0, FB;
-        // cout << "quantidade de nos = " << qNos << endl;
-
-        No *nosPtr[qNos];
-        int nosChaves[qNos];
-        getVetorAvr(raiz, nosChaves, nosPtr,q); //criar um vetor com a arvore, com raiz = raiz -> getVetorAvr()
-        // cout << "imprimindo os dados" << endl;
-        while(true){
-            
-            //iterar sobre os nos da arvore p recalcular o FB dos nos
-            for(i = 0; i<qNos; i++){
-                FB = CalcularFB(nosPtr[i]); // --------------- recalcular
-                nosPtr[i]->FB = FB;
-                // cout << "i = " << i << "  | chave = " << nosChaves[i] << "  | FB = " << nosPtr[i]->FB << endl;
-                //verificar se modulo eh igual a 2              
-                if(abs(FB) == 2){
-                    // cout << "no desbalanceado = " << " i = " << i << endl;
-                    noDesbalanceado = nosPtr[i];
-                    // cout << "no desbalanceado = " << noDesbalanceado << endl;
-                }
-            }          
-            if(noDesbalanceado != NULL){
-                No * filhoDir = noDesbalanceado->dir;
-                No * filhoEsq = noDesbalanceado->esq;
-                No * p = noDesbalanceado;
-                // cout << "\n\np = " << p->chave << endl;
-                // if(filhoDir != NULL)
-                //     cout << "filhoDir = " << filhoDir->chave << endl;
-                // if(filhoEsq != NULL)
-                //     cout << "filhoEsq = " << filhoEsq->chave << endl;
-                // balancear
-                if(noDesbalanceado->FB == 2){
-                    No * z = p->dir;
-                    if(filhoDir->FB < 0){
-                        No * y = z->esq;
-                        RotacaoDuplaEsquerda(p,z,y);
-                    }
-                    else{
-                        // cout << "z = " << z->chave << endl;
-                        RotacaoSimplesEsquerda(p,z);
-                    }
-                        
-                
-                }
-                else if(noDesbalanceado->FB == -2){
-                    No * u = filhoEsq;
-                    // cout << "u = " << u->chave << endl;
-                    if(filhoEsq->FB > 0){
-                        No * v = u->dir;
-                        RotacaoDuplaDireita(p,u,v);
-                    }
-                    else{
-                        RotacaoSimplesDireita(p,u);
-                    }
-                }
-                
-                noDesbalanceado =  NULL;
-            }
-            else 
-                break;
-        }
-        
-
-    }
-
-    void getVetorAvr(No* no, int nosChaves[], No *nosPtr[], int &q){
-        /*
-        inputs
-            no: nó da arvore
-            nosChaves: vetor com as chaves dos nós
-            nosPtr: vetor com os ponteiros dos nós
-            q: quantidade de iterações: idealmente colocar 0
-        outputs por parâmetro:
-            nosChaves
-            nosPtr
-        */
-            if(no != NULL)
-            {
-                getVetorAvr(no->getEsq(), nosChaves,nosPtr, q);
-                //cout << "q = " << q << "  | chave = " << no->getChave() <<  endl;
-                nosChaves[q] = no->getChave();
-                nosPtr[q] = no;
-                q+=1;
-                getVetorAvr(no->getDir(), nosChaves,nosPtr, q);
-
-            }
-        }
-
+    
     int CalcularFB(No * no)
     {
         if(no == NULL)
@@ -187,6 +97,129 @@ class Arvore
         }
     }
 
+    void getVetorAvr(No* no, No *nosPtr[], int &q){
+        /*
+        inputs
+            no: nó da arvore
+            nosPtr: vetor com os ponteiros dos nós
+            q: quantidade de iterações: idealmente colocar 0
+        outputs por parâmetro:
+            nosPtr
+        */
+            if(no != NULL)
+            {
+                getVetorAvr(no->getEsq(),nosPtr, q);
+                //cout << "q = " << q << "  | chave = " << no->getChave() <<  endl;
+                nosPtr[q] = no;
+                q+=1;
+                getVetorAvr(no->getDir(),nosPtr, q);
+
+            }
+        }
+
+    No* PesquisarPai(int dado, No* no){ // novo
+  	/*
+		retorna o pai de um nó.
+		OBS: colocar apenas nó com pai
+  	*/
+    if (raiz == NULL) return NULL; //arvore vazia
+    if (no->getChave() == dado) return NULL; //arvore com 1 folha, sem pai
+    No* atual = no;  // cria ptr aux (atual) e comeca a procurar
+    No* noFilhoDir = NULL;
+    No* noFilhoEsq = NULL;
+    while (true) {
+	  if(atual->getDir() != NULL)// verificar se tem chave na direita
+			if(atual->getDir()->getChave() == dado) //verifica se é o dado
+				return atual;
+	  if(atual->getEsq() != NULL)// verificar se tem chave na esquerda
+			if(atual->getEsq()->getChave() == dado) //verifica se é o dado
+				return atual;
+      if(dado < atual->getChave() )
+	  	atual = atual->getEsq(); // caminha para esquerda
+      else
+	  	atual = atual->getDir(); // caminha para direita
+      if (atual == NULL)
+	  	return NULL; // encontrou uma folha e nao encontrou a chave
+    }
+    return atual; //encontrou o dado
+  }
+
+
+    void Balancear(void){
+        /* Balancear a arvore
+        nosPtr: vetor com os ponteiros da arvore
+        nosChaves: vetor com as chaves da arvore
+        qNos: quantidade de nos na arvore
+        */
+        int i,valor, qNos = contarNos(raiz), q=0, FB;
+        // cout << "quantidade de nos = " << qNos << endl;
+
+        No *nosPtr[qNos];
+        getVetorAvr(raiz, nosPtr,q); //criar um vetor com a arvore, com raiz = raiz -> getVetorAvr()
+        // cout << "imprimindo os dados" << endl;
+        while(true){
+            //iterar sobre os nos da arvore p recalcular o FB dos nos
+            for(i = 0; i<qNos; i++){
+                FB = CalcularFB(nosPtr[i]); // --------------- recalcular
+                nosPtr[i]->FB = FB;
+                // cout << "i = " << i << "  | chave = " << nosChaves[i] << "  | FB = " << nosPtr[i]->FB << endl;
+                //verificar se modulo eh igual a 2              
+                if(abs(FB) == 2){
+                    // cout << "no desbalanceado = " << " i = " << i << endl;
+                    noDesbalanceado = nosPtr[i];
+                    noPaiDesbalanceado = PesquisarPai(noDesbalanceado->chave, raiz);
+                    cout << "no desbalanceado = " << noDesbalanceado->chave << endl;
+                }
+            }
+             
+            if(noDesbalanceado != NULL){
+                No * filhoDir = noDesbalanceado->dir;
+                No * filhoEsq = noDesbalanceado->esq;
+                No * p = noDesbalanceado;
+                No * pai = noPaiDesbalanceado;
+                // cout << "pai desbalanceado = " << pai->chave << endl;
+                
+                // cout << "\n\np = " << p->chave << endl;
+                // if(filhoDir != NULL)
+                //     cout << "filhoDir = " << filhoDir->chave << endl;
+                // if(filhoEsq != NULL)
+                //     cout << "filhoEsq = " << filhoEsq->chave << endl;
+                // balancear
+                if(noDesbalanceado->FB == 2){
+                    No * z = p->dir;
+                    if(filhoDir->FB < 0){
+                        No * y = z->esq;
+                        RotacaoDuplaEsquerda(p,z,y);
+                    }
+                    else{
+                       
+                        // cout << "z = " << z->chave << endl;
+                        RotacaoSimplesEsquerda(p,z);
+                        // DesenhaArvore(); break; 
+                    }
+                }
+                else if(noDesbalanceado->FB == -2){
+                    No * u = filhoEsq;
+                    // cout << "u = " << u->chave << endl;
+                    if(filhoEsq->FB > 0){
+                        No * v = u->dir;
+                        cout << "executa RotacaoDuplaDireita " << endl;
+                        RotacaoDuplaDireita(p,u,v);
+                    }
+                    else{
+                        RotacaoSimplesDireita(p,u);
+                    }
+                }
+                
+                noDesbalanceado =  NULL;
+                noPaiDesbalanceado =  NULL;
+            }
+            else 
+                break;
+        }
+        
+
+    }
 
     bool remover(int v)
     {
@@ -254,8 +287,10 @@ class Arvore
             else pai->setDir(sucessor); // se for filho a direita do pai
             sucessor->setEsq(atual->getEsq()); // acertando os ponteiros esquerda e direita do sucessor
         }
-        //delete atual;
+        // cout << "pai = " << pai->chave << "atual = " << atual->chave << endl;
+        delete atual;
         Balancear();
+
         return true;
     }
 
@@ -450,18 +485,31 @@ class Arvore
     //------------------------ rotacoes --------------------------
 
     void RotacaoSimplesDireita(No*& p, No*& u){//passar ponteiro por referencia
+        No * pPai = PesquisarPai(p->chave, raiz);
+        bool pFilhoEsq = pPai->esq == p; // p eh filho esq?     
+        
         p->setEsq(u->getDir());
         u->setDir(p);
         // ajusta o novo balanceamento de p
         // pois ele passa ser uma T1 - T2 = 0
         p->setFB(0);
         // p = u; // p passa ser o u
+
+        cout <<" => realizada uma rotacao simples a direita \n";
         if(raiz == p)
             raiz = u;
-        cout <<" => realizada uma rotacao simples a direita \n";
+        else{//p nao eh raiz
+            if(pFilhoEsq)
+                pPai->esq = u;
+            else
+                pPai->dir = u;
+        }
     }
 
     void RotacaoDuplaDireita(No*& p, No*& u, No*& v){
+        No * pPai = PesquisarPai(p->chave, raiz);
+        bool pFilhoEsq = pPai->esq == p; // p eh filho esq?   
+        
         v = u->getDir(); // aqui poderia ser o q caso T2 e T3 sejam nulos
         u->setDir(v->getEsq());
         p->setEsq(v->getDir());
@@ -479,13 +527,26 @@ class Arvore
             p->setFB(0);
             u->setFB(0);
         }
+        cout << " => realizada uma rotacao dupla a direita \n";
         // p = v; // p passa ser o v
         if(raiz == p)
             raiz = v;
-        cout << " => realizada uma rotacao dupla a direita \n";
+        else{//p nao eh raiz
+            if(pFilhoEsq)
+                pPai->esq = v;
+            else
+                pPai->dir = v;
+        }
     }
 
     void RotacaoSimplesEsquerda(No*& p, No*& z){
+        No * pPai = PesquisarPai(p->chave, raiz);
+        bool pFilhoEsq = pPai->esq == p; // p eh filho esq?
+        
+        // cout << "\n\n\ndesenha arvore " << endl;DesenhaArvore();
+        // cout << "p = " << p->chave << "   z = " << z->chave << endl;
+        // cout << "z.esq = " << z->getEsq()->chave << "   z.dir = " << z->getDir()->chave << endl;
+
         p->setDir(z->getEsq());
         z->setEsq(p);
         // ajusta o novo balanceamento de p
@@ -495,9 +556,18 @@ class Arvore
         cout << " => realizada uma rotacao simples a esquerda \n";
         if(raiz == p)
             raiz = z;
+        else{//p nao eh raiz
+            if(pFilhoEsq)
+                pPai->esq = z;
+            else
+                pPai->dir = z;
+        }
     }
 
     void RotacaoDuplaEsquerda(No*& p, No*& z, No*& y){
+        No * pPai = PesquisarPai(p->chave, raiz);
+        bool pFilhoEsq = pPai->esq == p; // p eh filho esq?           
+
         // y = z->getEsq();
         z->setEsq(y->getDir());
         p->setDir(y->getEsq());
@@ -517,10 +587,17 @@ class Arvore
                 p->setFB(0);
         }
         
+        cout << " => realizada uma rotacao dupla a esquerda \n";
         // p = y;
         if(raiz == p)
             raiz = y;
-        cout << " => realizada uma rotacao dupla a esquerda \n";
+        else{//p nao eh raiz
+            if(pFilhoEsq)
+                pPai->esq = y;
+            else
+                pPai->dir = y;
+        }
+
         // DesenhaArvore();
     }
     //-----------------------------------------------------
@@ -560,17 +637,97 @@ class Arvore
 int main(int argc, char *argv[])
 {
 	Arvore arv;
-	// TESTES do remover
+	// TESTES do remover sem pai
     // 1) RSE ok
-    // arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(55);arv.inserir(70);arv.remover(40); 
+    // arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(55);arv.inserir(70); arv.remover(40);
     // 2) RDE ok
     //  arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(55);arv.remover(40); 
     // 3) RSD ok
     //  arv.inserir(60); arv.inserir(50); arv.inserir(70);arv.inserir(40);arv.remover(70); 
     // 4) RDD ok
     //  arv.inserir(60); arv.inserir(50); arv.inserir(70);arv.inserir(55);arv.remover(70);
-    arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(55);arv.inserir(70);arv.remover(100);
+
+
+	// TESTES do remover com pai
+    
+    /**********************************
+    1) RSE -> remove(55) ok
+               50
+         40          60
+      30           55      70
+                        65    80
+
+               50
+         40            70
+      30           60      80
+                     65
+
+    arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(30);arv.inserir(55);
+    arv.inserir(70);arv.inserir(65);arv.inserir(80); 
     cout << "apos inserir" << endl; arv.DesenhaArvore();
+    arv.remover(55);
     cout << "\n\n\n\nDenhando a arvore deitada depois de balancear...\n"; arv.DesenhaArvore();
+    */
+
+    /**********************************
+    2) RDE ok -> remove(55)
+               50
+         40          60
+      30           55      70
+                         65    
+
+               50
+         40            65
+      30           60      70
+                     
+
+    arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(30);arv.inserir(55);
+    arv.inserir(70);arv.inserir(65);
+    cout << "apos inserir" << endl; arv.DesenhaArvore();
+    arv.remover(55);
+    cout << "\n\n\n\nDenhando a arvore deitada depois de balancear...\n"; arv.DesenhaArvore();
+    */
+
+    /*3) RSD -> remove(45)
+               50
+         40          60
+      30   45              70
+    20                         
+
+               50
+         30          60
+      20   40            70
+         
+                     
+
+    arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(30);arv.inserir(45);
+    arv.inserir(70);arv.inserir(20);
+    cout << "apos inserir" << endl; arv.DesenhaArvore();
+    arv.remover(45);
+    cout << "\n\n\n\nDenhando a arvore deitada depois de balancear...\n"; arv.DesenhaArvore();
+    */
+
+    /*4) RDD -> remove(45)
+               50
+         40          60
+     30     45              70
+       35                         
+
+               50
+         35          60
+     30     40              70
+           
+         
+                     
+
+    arv.inserir(50); arv.inserir(40); arv.inserir(60);arv.inserir(30);arv.inserir(45);
+    arv.inserir(70);arv.inserir(35);
+    cout << "apos inserir" << endl; arv.DesenhaArvore();
+    arv.remover(45);
+    cout << "\n\n\n\nDenhando a arvore deitada depois de balancear...\n"; arv.DesenhaArvore();
+    */
+
+
+
 	return 0;
 }
